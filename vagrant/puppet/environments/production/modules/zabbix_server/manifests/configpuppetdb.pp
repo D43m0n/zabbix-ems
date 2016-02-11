c9c7cdc441c3121ae34e59310ca8c9b742f6804e
D43m0n <dortsen@gmail.com>
2016-02-11 13:17:23 +0100

    Removed unused variables

--- vagrant/puppet/environments/production/modules/zabbix_server/manifests/configpuppetdb.pp
+++ vagrant/puppet/environments/production/modules/zabbix_server/manifests/configpuppetdb.pp
@@ -2,10 +2,8 @@
 class zabbix_server::configpuppetdb {
 
   $puppetcmd = '/opt/puppetlabs/puppet/bin/puppet'
-  $puppetdbcmd = '/opt/puppetlabs/server/apps/puppetdb/bin/puppetdb'
   $puppetconfig = '/etc/puppetlabs/puppet'
   $fullyqualified = $::facts['fqdn']
-  $hostname = $::facts['hostname']
 
   exec {'puppet-generate-certificate':
     path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
